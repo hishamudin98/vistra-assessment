8d34b0ac9c1cc04d1ad2ae3b5fd4b84e65e44e60
Hisham <hishamudin.ali@gmail.com>
2026-01-24 23:25:49 +0800

    add migrations pattern

---
+++ database/migrations/20260124151022_user_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(100) NOT NULL,
+    `first_name` VARCHAR(100) NULL,
+    `last_name` VARCHAR(100) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_username_key`(`username`),
+    INDEX `users_username_idx`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
