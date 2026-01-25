fe6bfb8ccd5d0fd8476f3db57155ccaa2885c642
Hisham <hishamudin.ali@gmail.com>
2026-01-25 16:59:10 +0800

    update ui component

--- frontend/components/ui/table.tsx
+++ frontend/components/ui/table.tsx
@@ -19,7 +19,7 @@ const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
+  <thead ref={ref} className={cn("bg-primary text-primary-foreground", className)} {...props} />
 ))
 TableHeader.displayName = "TableHeader"
 
@@ -72,7 +72,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-10 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+      "h-10 px-2 text-left align-middle font-medium bg-primary text-primary-foreground first:rounded-tl-md last:rounded-tr-md [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
       className
     )}
     {...props}
