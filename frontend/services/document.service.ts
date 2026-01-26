495930f01b13f0b32690db8a6fca3bf92761f923
Hisham <hishamudin.ali@gmail.com>
2026-01-26 22:51:21 +0800

    update service

--- frontend/services/document.service.ts
+++ frontend/services/document.service.ts
@@ -6,3 +6,18 @@ export const getDocuments = async (): Promise<any> => {
   const response = await axios.get(`${basePath}/documents`);
   return response.data;
 };
+
+export const createFolder = async (folder: any): Promise<any> => {
+  const response = await axios.post(`${basePath}/create-folder`, folder);
+  return response.data;
+};
+
+export const uploadFile = async (file: any): Promise<any> => {
+  const response = await axios.post(`${basePath}/upload-file`, file);
+  return response.data;
+};
+
+export const deleteDocument = async (id: string): Promise<any> => {
+  const response = await axios.delete(`${basePath}/documents/${id}`);
+  return response.data;
+};
